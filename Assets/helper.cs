255168c198c5074f058df19b5fc04be80d2c011f
ewanreis <87541930+ewanreis@users.noreply.github.com>
2021-10-12 00:48:10 +0100

    Update

--- Assets/helper.cs
+++ Assets/helper.cs
@@ -12,68 +12,62 @@ public class Helper : MonoBehaviour
     public static void InstantiateProjectile(GameObject obj, float xpos, float ypos, float xvel, float yvel)
     {   GameObject instance = Instantiate(obj, new Vector3(xpos, ypos, 0), Quaternion.identity);
         Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector3(xvel, yvel, 0); // Set Velocity      
+        rb.velocity = new Vector3(xvel, yvel, 0); // Set Velocity
         Destroy(instance, 3f); // Destroy Bullet after 3 seconds
-    } // Instantiate any projectile with a set Velocit
+    } // Instantiate any projectile with a set Velocity
     public static float ProjectileVelocity(bool up, bool down, bool left, bool right, char mode)
     {   float velocity = 0;
         if (mode == 'x')
-        {   if (up == true)           
-                velocity = 0f;           
-            if (right == true)           
-                velocity = 3f;        
-            if (left == true)            
-                velocity = -3f; 
-            if (down == true)           
+        {   if (up == true)
+                velocity = 0f;
+            if (right == true)
+                velocity = 3f;
+            if (left == true)
+                velocity = -3f;
+            if (down == true)
                 velocity = 0f;
         } else if (mode == 'y')
-        {   if (up == true)          
+        {   if (up == true)
                 velocity = 3f;
             if (right == true)
                 velocity = 0f;
-            if (left == true)           
-                velocity = 0;      
-            if (down == true)     
-                velocity = -3f;    
+            if (left == true)
+                velocity = 0;
+            if (down == true)
+                velocity = -3f;
         } return velocity;
     } public static string AnimationSwitcher(float x, float y, Animator anim)
-    {
-        string direction = "";
+    {   string direction = "";
         if (x == 0 && y == 0) // Idle
-        {
-            anim.SetBool("WalkUp", false);
+        {   anim.SetBool("WalkUp", false);
             anim.SetBool("WalkLeft", false);
             anim.SetBool("WalkRight", false);
             anim.SetBool("WalkDown", false);
             direction = "down";
         }
         if (x < -0.01) // Walk Left
-        {
-            anim.SetBool("WalkLeft", true);
+        {   anim.SetBool("WalkLeft", true);
             anim.SetBool("WalkRight", false);
             anim.SetBool("WalkUp", false);
             anim.SetBool("WalkDown", false);
             direction = "left";
         }
         else if (x > 0) // Walk Right
-        {
-            anim.SetBool("WalkLeft", false);
+        {   anim.SetBool("WalkLeft", false);
             anim.SetBool("WalkRight", true);
             anim.SetBool("WalkUp", false);
             anim.SetBool("WalkDown", false);
             direction = "right";
         }
         if (y < -0.01) // Walk Down
-        {
-            anim.SetBool("WalkDown", true);
+        {   anim.SetBool("WalkDown", true);
             anim.SetBool("WalkUp", false);
             anim.SetBool("WalkLeft", false);
             anim.SetBool("WalkRight", false);
             direction = "down";
         }
         else if (y > 0) // Walk Up
-        {
-            anim.SetBool("WalkUp", true);
+        {   anim.SetBool("WalkUp", true);
             anim.SetBool("WalkDown", false);
             anim.SetBool("WalkLeft", false);
             anim.SetBool("WalkRight", false);
@@ -99,7 +93,5 @@ public class Helper : MonoBehaviour
         // draw a debug ray to show ray position
         // You need to enable gizmos in the editor to see these
         Debug.DrawRay(Player.transform.position, -Vector2.up * rayLength, hitColor);
-
     }
-
 }
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -2,67 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class playerController : MonoBehaviour
 {   private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
     private RaycastHit2D hit;
-    private Animator anim;   
+    Rigidbody2D playerRigid;
+    private Animator anim;
     public GameObject Player, Bullet;
+
     public bool down, up, left, right;
     public float timer=0;
-    Rigidbody2D playerRigid;
+    
     private void Start()
     {   boxCollider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         playerRigid = GetComponent<Rigidbody2D>();
     }private void Update()
     {   if (Input.GetKey("space") && timer <= 0)
-        {   float xPosition = Player.transform.position.x, yPosition = Player.transform.position.y;
-            float xVelocity = Helper.ProjectileVelocity(up, down, left, right, 'x'), yVelocity = Helper.ProjectileVelocity(up, down, left, right, 'y');
+        {   float xPosition = Player.transform.position.x, yPosition = Player.transform.position.y
+            , xVelocity = Helper.ProjectileVelocity(up, down, left, right, 'x'), yVelocity = Helper.ProjectileVelocity(up, down, left, right, 'y');
             Helper.InstantiateProjectile(Bullet, xPosition, yPosition, xVelocity, yVelocity);
             timer = 20;
         }   timer--;
         Helper.DoRayCollisionCheck(Player);
     }private void FixedUpdate()
-    {   down = false; up = false; left = false; right=false;
-        float x = Input.GetAxisRaw("Horizontal"),y=0;
+    {   float x = Input.GetAxisRaw("Horizontal"),y=Input.GetAxisRaw("Vertical");
         if (x != 0)
             y = 0;
-        else if (x == 0)
-            y = Input.GetAxisRaw("Vertical");
-        
-        moveDelta = new Vector3(x, y, 0);                
+        moveDelta = new Vector3(x, y, 0);
+
         // Prevent Going inside colliders y
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y)
             , Mathf.Abs(moveDelta.y * Time.deltaTime)
-            , LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)           
+            , LayerMask.GetMask("Enemy", "Blocking"));
+        if (hit.collider == null)
             transform.Translate(0, moveDelta.y * Time.deltaTime, 0); // Movement
 
         // Prevent Going inside colliders x
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.x)
             , Mathf.Abs(moveDelta.x * Time.deltaTime)
-            , LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)            
+            , LayerMask.GetMask("Enemy", "Blocking"));
+        if (hit.collider == null)
             transform.Translate(moveDelta.x * Time.deltaTime, 0, 0); // Movement
-        string direction = Helper.AnimationSwitcher(x, y, anim);
-        switch (direction)
-        {   case "up":
-                up = true;
-                break;
-            case "down":
-                down = true;
-                break;
-            case "left":
-                left = true;
-                break;
-            case "right":
-                right = true;
-                break;
-            default:
-                down = true;
-                break;
-        }
-    }   
 
+        down = false; up = false; left = false; right=false;
+        string direction = Helper.AnimationSwitcher(x, y, anim);
+        if (direction =="up")
+        up = true;
+        if (direction =="down")
+        down = true;
+        if (direction =="left")
+        left = true;
+        if (direction =="right")
+        right = true;
+        else
+        down = true;
+    }
 }
