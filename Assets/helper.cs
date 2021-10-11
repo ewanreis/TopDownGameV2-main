8500f9deab59f04fccf3cdb13f4e214857a43764
ewanreis <87541930+ewanreis@users.noreply.github.com>
2021-10-11 12:22:04 +0100

    Update

--- Assets/helper.cs
+++ Assets/helper.cs
@@ -1,19 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Helper : MonoBehaviour
-{
+{ 
     public static void FlipSprite(GameObject obj, bool flipLeft)
-    {
-        if (flipLeft == true)
+    {   if (flipLeft == true)
             obj.transform.localRotation = Quaternion.Euler(0, 180, 0);
         else
             obj.transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
-    void Update()
-    {
-        //helper.FlipSprite(gameObject, true);
-        //helper.FlipSprite(gameObject, false);
+    public static void InstantiateProjectile(GameObject obj, float xpos, float ypos, float xvel, float yvel)
+    {   GameObject instance = Instantiate(obj, new Vector3(xpos, ypos, 0), Quaternion.identity);
+        Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(xvel, yvel, 0); // Set Velocity      
+        Destroy(instance, 3f); // Destroy Bullet after 3 seconds
+    } // Instantiate any projectile with a set Velocit
+    public static float ProjectileVelocity(bool up, bool down, bool left, bool right, char mode)
+    {   float velocity = 0;
+        if (mode == 'x')
+        {   if (up == true)           
+                velocity = 0f;           
+            if (right == true)           
+                velocity = 3f;        
+            if (left == true)            
+                velocity = -3f; 
+            if (down == true)           
+                velocity = 0f;
+        } else if (mode == 'y')
+        {   if (up == true)          
+                velocity = 3f;
+            if (right == true)
+                velocity = 0f;
+            if (left == true)           
+                velocity = 0;      
+            if (down == true)     
+                velocity = -3f;    
+        } return velocity;
+    } public static void AnimationSwitcher(float x, float y, Animator anim)
+    {   if (x == 0 && y == 0) // Idle
+        {
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", false);
+            anim.SetBool("WalkDown", false);
+        }
+        if (x < -0.01) // Walk Left
+        {
+            anim.SetBool("WalkLeft", true);
+            anim.SetBool("WalkRight", false);
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkDown", false);
+
+        }
+        else if (x > 0) // Walk Right
+        {
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", true);
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkDown", false);
+
+        }
+        if (y < -0.01) // Walk Down
+        {
+            anim.SetBool("WalkDown", true);
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", false);
+
+        }
+        else if (y > 0) // Walk Up
+        {
+            anim.SetBool("WalkUp", true);
+            anim.SetBool("WalkDown", false);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", false);
+
+        }
     }
 }
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -7,31 +7,20 @@ public class playerController : MonoBehaviour
     private Vector3 moveDelta;
     private RaycastHit2D hit;
     private Animator anim;   
-    public GameObject Player;
-    public GameObject Bullet;
+    public GameObject Player, Bullet;
     public bool down, up, left, right;
     public float timer=0;
+    Rigidbody2D playerRigid;
     private void Start()
     {   boxCollider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+        playerRigid = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {   if (Input.GetKey("space") && timer <= 0)
         {   float xPosition = Player.transform.position.x, yPosition = Player.transform.position.y;
-            float xVelocity = 0f, yVelocity = 0f;
-            if (up == true)
-            {   xVelocity = 0f;
-                yVelocity = 3f;
-            }if (right == true)
-            {   xVelocity = 3f;
-                yVelocity = 0f;
-            }if (left == true)
-            {   xVelocity = -3f;
-                yVelocity = 0;
-            }if (down == true)
-            {   xVelocity = 0f;
-                yVelocity = -3f;
-            } MakeBullet(Bullet, xPosition, yPosition, xVelocity, yVelocity);
+            float xVelocity = Helper.ProjectileVelocity(up, down, left, right, 'x'), yVelocity = Helper.ProjectileVelocity(up, down, left, right, 'y');
+            Helper.InstantiateProjectile(Bullet, xPosition, yPosition, xVelocity, yVelocity);
             timer = 20;
         }   timer--;
     }
@@ -41,7 +30,8 @@ public class playerController : MonoBehaviour
         if (x != 0)
             y = 0;
         else if (x == 0)
-            y = Input.GetAxisRaw("Vertical");        
+            y = Input.GetAxisRaw("Vertical");
+
         moveDelta = new Vector3(x, y, 0);                
         // Prevent Going inside colliders y
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y)
@@ -49,49 +39,14 @@ public class playerController : MonoBehaviour
             , LayerMask.GetMask("Actor", "Blocking"));
         if (hit.collider == null)           
             transform.Translate(0, moveDelta.y * Time.deltaTime, 0); // Movement
+
         // Prevent Going inside colliders x
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.x)
             , Mathf.Abs(moveDelta.x * Time.deltaTime)
             , LayerMask.GetMask("Actor", "Blocking"));
         if (hit.collider == null)            
             transform.Translate(moveDelta.x * Time.deltaTime, 0, 0); // Movement
-        if (x == 0 && y == 0) // Idle
-        {   anim.SetBool("WalkUp", false);
-            anim.SetBool("WalkLeft", false);
-            anim.SetBool("WalkRight", false);
-            anim.SetBool("WalkDown", false);
-            down = true;
-        } if ( x < -0.01 ) // Walk Left
-        {   anim.SetBool("WalkLeft", true);
-            anim.SetBool("WalkRight", false);
-            anim.SetBool("WalkUp", false);
-            anim.SetBool("WalkDown", false);
-            left = true;
-        } else if ( x > 0 ) // Walk Right
-        {   anim.SetBool("WalkLeft", false);
-            anim.SetBool("WalkRight", true);
-            anim.SetBool("WalkUp", false);
-            anim.SetBool("WalkDown", false);
-            right = true;
-        } if (y < 0) // Walk Down
-        {   anim.SetBool("WalkDown", true);
-            anim.SetBool("WalkUp", false);
-            anim.SetBool("WalkLeft", false);
-            anim.SetBool("WalkRight", false);
-            down = true;
-        } else if (y > 0) // Walk Up
-        {   anim.SetBool("WalkUp", true);
-            anim.SetBool("WalkDown", false);
-            anim.SetBool("WalkLeft", false);
-            anim.SetBool("WalkRight", false);
-            up = true;
-        } 
+        Helper.DirectionalSwitcher(x, y, anim);
     }   
-    public static void MakeBullet(GameObject prefab, float xpos, float ypos, float xvel, float yvel)
-    {   GameObject instance = Instantiate(prefab, new Vector3(xpos, ypos, 0), Quaternion.identity);       
-        Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector3(xvel, yvel, 0); // Set Velocity
-        // Destroy Bullet after 3 seconds
-        Destroy(instance, 3f);
-    } 
+
 }
